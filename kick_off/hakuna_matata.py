faff3cb73e968a7ee767e1f35f35e12f334bdc69
juniorschmitz <juniorschmitz9@gmail.com>
2018-09-04 22:53:21 -0300

    Adding python automation kick-off to do some tests later

---
+++ kick_off/hakuna_matata.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("https://www.google.com")
+lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+lucky_button.click()
+
+# capture the screen
+driver.get_screenshot_as_file("capture.png")
